fe74994b01475370437452110de7ea0bc1904715
akemilol <valeriaconcedsantos@gmail.com>
2024-10-25 19:02:02 -0300

    Inicialização da route.ts

--- cp6/src/app/api/base-checkpoint/route.ts
+++ cp6/src/app/api/base-checkpoint/route.ts
@@ -1,11 +1,18 @@
-import { NextResponse } from "next/server";
-import { promises as fs } from "fs";
-import { TipoCheckpoint } from "@/types";
+// import { NextResponse } from "next/server";
+// import { promises as fs } from "fs";
+// import { TipoCheckpoint } from "@/types";
 
-export async function GET() {
-    const file = await fs.readFile(process.cwd() + "/src/data/base.json" , "utf-8");
+// export async function GET() {
+//     const file = await fs.readFile(process.cwd() + "/src/data/base.json" , "utf-8");
 
-    //PARSEAR O ARQUIVO
-    const checkpoints = JSON.parse(file);
-    return NextResponse.json(checkpoints);
-}
+//     const checkpoints = JSON.parse(file);
+//     return NextResponse.json(checkpoints);
+// }
+
+// export async function POST(request:Request) {
+//     try{
+//         const file = await fs.readFile(process.cwd() + "/src/data/base.json" , "utf-8");
+//     }
+// }
+
+// parte do eduardo!
