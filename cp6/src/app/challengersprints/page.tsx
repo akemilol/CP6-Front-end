26410d986b6e0e8e928d2fcb8c832d8b7dfbdfa8
mirelapsr <mirelarodrigues05@gmail.com>
2024-10-27 19:33:42 -0300

    correção page challenge

--- cp6/src/app/challengersprints/page.tsx
+++ cp6/src/app/challengersprints/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function Challenges() {
 
-    const [challenge, setCheckpoint] = useState<TipoChallenge[]>([]);
+    const [challenge, setChallenge] = useState<TipoChallenge[]>([]);
     const [alunoSelecionado, setAlunoSelecionado] = useState<string>("Todos");
 
     useEffect(() => {
